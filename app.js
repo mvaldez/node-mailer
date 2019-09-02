b2dbe7a6e4297098da036fc39758303b28fbe063
Mark Valdez <mark.valdez@rmn.com>
2019-09-01 22:26:42 -0500

    send mail via gmail smtp

---
+++ app.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs')
+
+const getOptions = function(filename) {
+    try {
+        return JSON.parse(fs.readFileSync(filename).toString())
+    } catch (e) {
+        throw e
+    }
+}
+
+nodemailer.createTestAccount((err, account) => {
+    const creds = getOptions('credentials.json')
+    const options = getOptions('mailOptions.json')
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.googlemail.com', // Gmail Host
+        port: 465,
+        secure: true,
+        auth: {
+            user: creds.user,
+            pass: creds.pass
+        }
+    });
+
+    let mailOptions = {
+        from: options.from,
+        to: options.to,
+        subject: options.subject,
+        text: options.text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+});
